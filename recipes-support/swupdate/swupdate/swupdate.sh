60c8e89edf95f3c34f8944235788671ac320864d
Stefano Babic <sbabic@denx.de>
2020-07-06 17:36:49 +0200

    Fix start of daemon in case of MULTILIB
    In case of multilib, swupdate.sh is put on /usr/lib64 instead of
    /usr/lib. Replace the path during the install.
    
    Signed-off-by: Stefano Babic <sbabic@denx.de>

--- recipes-support/swupdate/swupdate/swupdate.sh
+++ recipes-support/swupdate/swupdate/swupdate.sh
@@ -8,11 +8,11 @@ SWUPDATE_SURICATTA_ARGS=""
 
 # source all files from /etc/swupdate/conf.d and /usr/lib/swupdate/conf.d/
 # A file found in /etc replaces the same file in /usr
-for f in `(test -d /usr/lib/swupdate/conf.d/ && ls -1 /usr/lib/swupdate/conf.d/; test -d /etc/swupdate/conf.d && ls -1 /etc/swupdate/conf.d) | sort -u`; do
+for f in `(test -d @LIBDIR@/swupdate/conf.d/ && ls -1 @LIBDIR@/swupdate/conf.d/; test -d /etc/swupdate/conf.d && ls -1 /etc/swupdate/conf.d) | sort -u`; do
   if [ -f /etc/swupdate/conf.d/$f ]; then
     . /etc/swupdate/conf.d/$f
   else
-    . /usr/lib/swupdate/conf.d/$f
+    . @LIBDIR@/swupdate/conf.d/$f
   fi
 done
 
